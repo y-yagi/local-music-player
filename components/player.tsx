64d8da7e8da0f849b69a3eb8339cb8ff73db2794
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2020-09-30 13:45:56 +0900

    Adjust design

--- components/player.tsx
+++ components/player.tsx
@@ -48,11 +48,13 @@ const Player = (props: any) => {
       <section>
         <div className="md:flex">
           <div className="mt-4 md:mt-0 md:ml-6">
-            <div className="uppercase tracking-wide text-sm text-indigo-600 font-bold">
-              Playing
-            </div>
-            <div className="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline space-x-4">
-              {props.title}
+            <div className="flex space-x-4">
+              <span className="uppercase tracking-wide text-sm text-indigo-600 font-bold">
+                Playing
+              </span>
+              <span className="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline space-x-4">
+                {props.title}
+              </span>
               <button
                 className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full"
                 onClick={() => action()}
