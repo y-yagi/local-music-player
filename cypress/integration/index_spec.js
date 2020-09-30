7fb690867dcf1698e670f14735477db5960e5ca0
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2020-09-30 13:21:54 +0900

    Fix tests

--- cypress/integration/index_spec.js
+++ cypress/integration/index_spec.js
@@ -1,12 +1,6 @@
-import { setUserCookie } from "../../lib/userCookies";
-
-beforeEach(() => {
-  setUserCookie({ id: "123", email: "abc@example.com" });
-});
-
 describe("Index", () => {
   it("Show page", () => {
     cy.visit("http://localhost:3000");
-    cy.contains("Okini");
+    cy.contains("Local file music player");
   });
 });
