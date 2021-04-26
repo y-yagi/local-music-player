e2dacc76a0cac06154aff36a3c9aac2b14c64563
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2021-04-27 08:20:18 +0900

    Fix warnings

--- components/musics.tsx
+++ components/musics.tsx
@@ -16,7 +16,6 @@ const Musics = () => {
 
   async function fetchData() {
     const db = new MusicDatase();
-    console.log(sort);
     try {
       const musics = await db.musics.orderBy("id").toArray();
       if (sort === "title") {
@@ -68,7 +67,7 @@ const Musics = () => {
       <hr />
 
       <div className="flex justify-center">
-        <Link href="?sort=title">
+        <Link href="/?sort=title">
           <a>Sort by Title</a>
         </Link>
       </div>
